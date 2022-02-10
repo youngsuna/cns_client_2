56f6127cce3e09688d1c31a5b77407a6f8a8b683
youngsuna <youngsunas2@gmail.com>
2022-02-10 15:48:38 +0900

    client2 : Order Service client

--- src/main/java/com/share/boot/EurekaClient2Application.java
+++ src/main/java/com/share/boot/EurekaClient2Application.java
@@ -1,15 +1,23 @@
 package com.share.boot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class EurekaClient2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClient2Application.class, args);
 	}
 
+	@GetMapping("/order/info")
+	public String info(@Value("${server.port}") String port) {
+		return "Order 서비스의 기본 동작 Port: {" + port + "}";
+	}
 }
